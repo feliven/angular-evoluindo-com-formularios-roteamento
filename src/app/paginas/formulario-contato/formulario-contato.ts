e2dff21278001614f10fdc37a72851901f2b6626
FVS <feliven@gmail.com>
2025-10-15 09:02:06 -0300

    formulário com two-way binding

--- src/app/paginas/formulario-contato/formulario-contato.ts
+++ src/app/paginas/formulario-contato/formulario-contato.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
 import { Container } from '../../componentes/container/container';
 import { Separador } from '../../componentes/separador/separador';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-contato',
-  imports: [Container, Separador],
+  imports: [Container, Separador, FormsModule, ReactiveFormsModule],
   templateUrl: './formulario-contato.html',
   styleUrl: './formulario-contato.css',
 })
-export class FormularioContato {}
+export class FormularioContato {
+  formularioContato!: FormGroup;
+
+  constructor() {
+    this.formularioContato = new FormGroup({
+      nome: new FormControl('Nombre'),
+      telefone: new FormControl('3422334'),
+      email: new FormControl('e@mail.com'),
+      aniversario: new FormControl('02/02/2020'),
+      redes: new FormControl('twiter'),
+      observacoes: new FormControl('hello world'),
+    });
+  }
+}
